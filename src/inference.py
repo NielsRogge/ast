aefc8d3adb374fd8f8a0d6ab9865244191fd3959
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-10-02 16:00:52 +0200

    Fix bug

--- src/inference.py
+++ src/inference.py
@@ -3,9 +3,9 @@ from models import ASTModel
 
 from huggingface_hub import hf_hub_download
 
-# assume each input spectrogram has 100 time frames
-input_tdim = 100
-# create a pseudo input: a batch of 10 spectrogram, each with 100 time frames and 128 frequency bins
+# assume each input spectrogram has 1024 time frames
+input_tdim = 1024
+# create a pseudo input: a batch of 10 spectrogram, each with 1024 time frames and 128 frequency bins
 test_input = torch.rand([10, input_tdim, 128])
 
 # create an AST model
@@ -27,4 +27,4 @@ with torch.no_grad():
     output = model(test_input)
 
 # output should be in shape [10, 527], i.e., 10 samples, each with prediction of 527 classes.
-print(output.shape)
+print("Shape of the logits:", output.shape)

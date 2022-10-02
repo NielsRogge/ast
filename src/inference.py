cae98f8f32e10d19234f6131f9077d5d86e8800b
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-10-02 15:58:29 +0200

    Fix bug

--- src/inference.py
+++ src/inference.py
@@ -5,13 +5,11 @@ from huggingface_hub import hf_hub_download
 
 # assume each input spectrogram has 100 time frames
 input_tdim = 100
-# assume the task has 527 classes
-label_dim = 527
 # create a pseudo input: a batch of 10 spectrogram, each with 100 time frames and 128 frequency bins
 test_input = torch.rand([10, input_tdim, 128])
 
 # create an AST model
-model = ASTModel(label_dim=label_dim, input_tdim=input_tdim, imagenet_pretrain=False, audioset_pretrain=False)
+model = ASTModel(label_dim=527, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, imagenet_pretrain=False, audioset_pretrain=False, model_size='base384', verbose=False)
 
 # load pretrained weights
 filepath = hf_hub_download(repo_id="nielsr/audio-spectogram-transformer-checkpoint",

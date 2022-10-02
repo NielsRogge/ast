4e8a92011738af7f08c5c36616cd9d36cfb52986
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-10-02 16:07:24 +0200

    Fix bug

--- src/inference.py
+++ src/inference.py
@@ -1,12 +1,39 @@
-import torch
-from models import ASTModel
-
+import os
 from huggingface_hub import hf_hub_download
 
-# assume each input spectrogram has 1024 time frames
-input_tdim = 1024
-# create a pseudo input: a batch of 10 spectrogram, each with 1024 time frames and 128 frequency bins
-test_input = torch.rand([10, input_tdim, 128])
+import torch
+import torchaudio
+
+from models import ASTModel
+
+
+def make_features(wav_name, mel_bins, target_length=1024):
+    waveform, sr = torchaudio.load(wav_name)
+
+    fbank = torchaudio.compliance.kaldi.fbank(
+        waveform, htk_compat=True, sample_frequency=sr, use_energy=False,
+        window_type='hanning', num_mel_bins=mel_bins, dither=0.0,
+        frame_shift=10)
+
+    n_frames = fbank.shape[0]
+
+    p = target_length - n_frames
+    if p > 0:
+        m = torch.nn.ZeroPad2d((0, 0, 0, p))
+        fbank = m(fbank)
+    elif p < 0:
+        fbank = fbank[0:target_length, :]
+
+    fbank = (fbank - (-4.2677393)) / (4.5689974 * 2)
+    return fbank
+
+# create dummy audio
+filepath = hf_hub_download(repo_id="nielsr/audio-spectogram-transformer-checkpoint",
+                           filename="sample_audio.flac",
+                           repo_type="dataset")
+
+feats = make_features('/content/ast/sample_audios/sample_audio.flac', mel_bins=128) # shape(1024, 128)
+dummy_input = feats.expand(1, 1024, 128)                       
 
 # create an AST model
 model = ASTModel(label_dim=527, fstride=10, tstride=10, input_fdim=128, input_tdim=1024, imagenet_pretrain=False, audioset_pretrain=False, model_size='base384', verbose=False)
@@ -24,7 +51,8 @@ for key in list(state_dict.keys()):
 model.load_state_dict(state_dict)
 
 with torch.no_grad():
-    output = model(test_input)
+    output = model(dummy_input)
 
 # output should be in shape [10, 527], i.e., 10 samples, each with prediction of 527 classes.
 print("Shape of the logits:", output.shape)
+print("Predicted class:", output.argmax(-1)
